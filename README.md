f93bc018ddd9214073a28e57e432dcd1b78a752d
proin <proin@season.co.kr>
2022-08-12 15:45:14 +0900

    [update] daemon support

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Introduction
 
-SEASON Dizest Platform (dizest) provides visual convenience for artificial intelligence data analysis based on web technology.
+SEASON DIZESThub Platform (dizest) provides visual convenience for artificial intelligence data analysis based on web technology.
 
 ![screenshot](./screenshots/dizest.gif)
 
@@ -11,8 +11,22 @@ SEASON Dizest Platform (dizest) provides visual convenience for artificial intel
 ```bash
 pip install dizest
 cd <your-workspace>
-dizest run
+dizest install
+```
+
+## Start dizest server
+
+- run server
+
+```bash
 dizest run --port 4000 --host 0.0.0.0
+```
+
+- run server as daemon
+
+```bash
+dizest server start # start dizest server daemon
+dizest server stop  # stop dizest server daemon
 ```
 
 ## Upgrade
@@ -25,23 +39,13 @@ dizest update
 
 ## Roadmap
 
-- Core
-    - run entire workflow
-    - kernel for k8s
-    - cache controller
-    - command line tools (run workflow on terminal)
+- Spawner
+    - spawner for k8s
 - General
-    - User's mypage (for password change)
     - SAML Authentication
-- Apps
-    - copy apps from hub to myapps
-    - export/import app
 - Workflow
-    - auto process (auto ordering flows)
-- Explore
-    - explore workflow on hub
-    - comment or review functions
-    - copy workflow to my workflow
+    - export/import app
+    - upgrade display function (full function of jupyter)
 - Drive
     - connect with Google Drive, Dropbox, etc.
 - Dizest Cloud
@@ -49,6 +53,10 @@ dizest update
     - connect with hub by API
 
 ## Release Note
+
+### 3.0.5
+
+- support dizest server command (daemon)
 
 ### 3.0.4
 
@@ -68,79 +76,3 @@ dizest update
 - update workflow editor ui: `codeflow`, `kernel selector`, etc
 - multi kernel support (support multi language like R)
 - support 3rd party development (spawner, kernel)
-
-### 2.2.1
-
-- wiz 1.0 upgrade
-
-### 2.2.0
-
-- ui changed (app editor/info)
-- cdn config in app editor
-- input/output ordering by drag in app editor
-- drive download url changed (apache bug)
-- db changed
-
-### 2.1.5
-
-- kernel config at dizest.json
-
-### 2.1.4
-
-- variable type on workflow apps (select, text, textarea, date, number)
-
-### 2.1.3
-
-- socketio bug fixed
-
-### 2.1.2
-
-- class bug fixed in workflow
-
-### 2.1.1
-- bug fixed
-
-### 2.1.0
-- pip package installer on admin page
-- change main page: apps -> workflow
-- bug fixed on process management
-- code refactoring
-- kernel upgrade (fork -> spawn)
-- add socketio config (config/socketio.py)
-
-### 2.0.8
-- workflow import/export
-- kill processes on admin dashboard
-- ui bug fixed on workflow
-
-### 2.0.7
-- display error status on workflow
-- code edit on workflow
-
-### 2.0.6
-- bug fixed
-
-### 2.0.5
-- app ui api bug fixed
-- display workflow error message
-- workflow running status logic changed
-
-### 2.0.4
-- dizest runner update (--host, --port option)
-- change dizest app process cwd (to local drive)
-- run shortcut on app development page (shift+enter)
-
-### 2.0.3
-- bug fixed
-
-### 2.0.2
-- bug fixed
-
-### 2.0.1
-- bug fixed
-
-### 2.0.0
-- Upgrade Workflow Engine
-- Upgrade App development API
-- Change UI/UX
-- adding Drive Concept
--- src/dizest/command/run.py
+++ src/dizest/command/run.py
@@ -11,16 +11,114 @@ from watchdog.events import FileSystemEventHandler
 import datetime
 import platform
 import signal
+import atexit
+import contextlib
+import multiprocessing as mp
 
 PATH_WIZ = season.path.lib
 PATH_DIZEST = os.path.dirname(os.path.dirname(__file__))
 
 PATH_WORKINGDIR = os.getcwd()
+PATH_WORKINGDIR_PID = os.path.join(PATH_WORKINGDIR, "dizest.pid")
 PATH_WORKINGDIR_WEBSRC = os.path.join(PATH_WORKINGDIR, "websrc")
 PATH_WORKINGDIR_PACKAGE = os.path.join(PATH_WORKINGDIR, "dizest.json")
 PATH_WORKINGDIR_CONFIG = os.path.join(PATH_WORKINGDIR, "config.py")
 
 cache = dict()
+
+class Daemon:
+    def __init__(self, pidfile, target=None, stdout='/dev/null', stderr='/dev/null'):
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+        self.run = target
+       
+    def daemonize(self):
+        try:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)
+        except OSError as e:
+            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        os.chdir("/")
+        os.setsid()
+        os.umask(0)
+
+        try:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)
+        except OSError as e:
+            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+        so = open(self.stdout, 'w')
+        se = open(self.stderr, 'w')
+        contextlib.redirect_stdout(so)
+        contextlib.redirect_stderr(se)
+
+        # write pidfile
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        open(self.pidfile, 'w').write("%s\n" % pid)
+
+    def delpid(self):
+        os.remove(self.pidfile)
+ 
+    def start(self):
+        try:
+            pf = open(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if pid:
+            message = "pidfile %s already exist. Daemon already running?\n"
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+        
+        self.daemonize()
+        self.run()
+
+    def stop(self):
+        try:
+            pf = open(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if not pid:
+            message = "pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self.pidfile)
+            return
+
+        try:
+            counter = 0
+            for child in psutil.Process(pid).children(recursive=True):
+                counter = counter + 1
+                child.kill()
+            print(f"killed {counter} dizest subprocess")
+        except:
+            pass
+
+        try:
+            while 1:
+                os.kill(pid, signal.SIGTERM)
+                time.sleep(0.1)
+        except OSError as err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                sys.exit(1)
 
 class Instance:
     def __init__(self, data):
@@ -109,6 +207,25 @@ class Instance:
         cwd = os.path.join(cwd, *path)
         return dizest.util.os.storage(cwd)
 
+def runnable():
+    publicpath = os.path.join(PATH_WORKINGDIR_WEBSRC, 'public')
+    apppath = os.path.join(publicpath, 'app.py')
+
+    while True:
+        try:
+            env = os.environ.copy()
+            env['WERKZEUG_RUN_MAIN'] = 'true'
+            process = subprocess.Popen([str(sys.executable), str(apppath)], env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process.communicate()
+        except Exception as e:
+            pass
+        except:
+            counter = 0
+            for child in psutil.Process(os.getpid()).children(recursive=True):
+                counter = counter + 1
+                child.kill()
+            sys.exit(0)
+
 @arg('--host', default=None, help='0.0.0.0')
 @arg('--port', default=0, help='3000')
 @arg('-f', default=None, help='workflow.dzw')
@@ -138,7 +255,7 @@ def run(f=None, host="0.0.0.0", port=0):
     port = int(port)
     fs = dizest.util.os.storage(PATH_WORKINGDIR_WEBSRC)
     if fs.exists() is False:
-        print("dizest not installed")
+        print("dizest not installed. run `dizest install` first.")
         return
     
     config = fs.read.json("dizest.json", dict())
@@ -181,19 +298,74 @@ def run(f=None, host="0.0.0.0", port=0):
         print("Invalid Project path: dizest structure not found in this folder.")
         return
 
-    def run_ctrl():
-        env = os.environ.copy()
-        env['WERKZEUG_RUN_MAIN'] = 'true'
-        process = subprocess.Popen([str(sys.executable), str(apppath)], env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(f"DIZEST Hub running on http://{host}:{port}")
-        process.communicate()
+    print(f"DIZEST Hub running on http://{host}:{port}")
+    runnable()
 
-    while True:
+@arg('action', default=None, help="start|stop|restart")
+@arg('--host', default=None, help='0.0.0.0')
+@arg('--port', default=0, help='3000')
+def server(action, host="0.0.0.0", port=0):
+    port = int(port)
+    fs = dizest.util.os.storage(PATH_WORKINGDIR_WEBSRC)
+    if fs.exists() is False:
+        print("dizest not installed. run `dizest install` first.")
+        return
+    
+    config = fs.read.json("dizest.json", dict())
+    
+    if port < 1000:
+        if 'port' in config:
+            port = int(config['port'])
+        else:
+            port = 3000
+    
+    # set host
+    if 'host' in config and host is None: host = config['host']
+    if host is None: host = '0.0.0.0'
+
+    config['port'] = port
+    config['host'] = host
+    config['path'] = PATH_WORKINGDIR
+
+    # save dizest config
+    fs.write.json("dizest.json", config)
+
+    # build config
+    PATH_CONFIG_BASE = os.path.join(PATH_DIZEST, 'res', 'wiz', 'server.py')
+    PATH_CONFIG = os.path.join(PATH_WORKINGDIR_WEBSRC, 'config', 'server.py')
+
+    data = fs.read.text(PATH_CONFIG_BASE)
+    data = data.replace("__PORT__", str(port))
+    data = data.replace("__HOST__", str(host))
+    fs.write.text(PATH_CONFIG, data)
+    
+    # copy config
+    fs.copy(os.path.join(PATH_DIZEST, 'res', 'wiz', 'wiz.py'), os.path.join("config", "wiz.py"))
+    fs.copy(os.path.join(PATH_DIZEST, 'res', 'wiz', 'socketio.py'), os.path.join("config", "socketio.py"))
+
+    # run server
+    publicpath = os.path.join(PATH_WORKINGDIR_WEBSRC, 'public')
+    apppath = os.path.join(publicpath, 'app.py')
+
+    if os.path.isfile(apppath) == False:
+        print("Invalid Project path: dizest structure not found in this folder.")
+        return
+
+    daemon = Daemon(PATH_WORKINGDIR_PID, target=runnable)
+    if action == 'start':
+        print(f"DIZESThub server running on http://{host}:{port}")
+        daemon.start()
+    elif action == 'stop':
+        print(f"DIZESThub server stop")
+        daemon.stop()
+    elif action == 'restart':
+        print("stopping running DIZESThub server...")
         try:
-            run_ctrl()
-        except Exception:
-            pass
+            daemon.stop()
         except:
-            for child in psutil.Process(os.getpid()).children(recursive=True):
-                child.kill()
-            sys.exit(0)
+            pass
+        
+        print(f"DIZESThub server restarted at http://{host}:{port}")
+        daemon.start()
+    else:
+        print(f"`dizest server` not support `{action}`. (start|stop|restart)")
